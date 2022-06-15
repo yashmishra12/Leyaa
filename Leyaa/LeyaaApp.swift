2291a971304bcdc56bc1d210eb1547085dd73d03
Yash Mishra <yashmishra12@hotmail.com>
2022-06-14 22:23:54 -0400

    Initial Commit

---
+++ Leyaa/LeyaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeyaaApp.swift
+//  Leyaa
+//
+//  Created by Yash Mishra on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LeyaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
