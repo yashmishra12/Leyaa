0ae4f1b2c96148132c3e0379217c325a35dcf08e
Yash Mishra <yashmishra12@hotmail.com>
2022-06-29 01:16:19 -0400

    Profile Page Updated. Added Sign Out there.

--- Leyaa/Components/Items/ItemSearchView.swift
+++ Leyaa/Components/Items/ItemSearchView.swift
@@ -10,23 +10,40 @@ import SwiftUI
 struct ItemSearchView: View {
     let names = assetName.sorted()
     @State private var searchText = ""
+    @Binding var recentlyDeleted: [String]
 
     var body: some View {
-        ZStack{
-                List {
-                    ForEach(searchResults, id: \.self) { name in
-                        NavigationLink(destination: Text(name)) {
-                            Text(name)
+        VStack {
+          
+            if recentlyDeleted.count>0 {
+                VStack{
+                    Text("Quick Undo").font(.title2).foregroundColor(.blue)
+                    HStack {
+                        ForEach(recentlyDeleted, id: \.self) { item in
+                            Image(item.sanitiseItemName()).resizable().frame(width: 50, height: 50)
                         }
                     }
                 }
-                .searchable(text: $searchText) {
-                    ForEach(searchResults, id: \.self) { result in
-                        Text(result).searchCompletion(result)
-                    }
-                }
-                .navigationTitle("Items")
+            }
             
+            ZStack{
+                 
+
+                    List {
+                        ForEach(searchResults, id: \.self) { name in
+                            NavigationLink(destination: Text(name)) {
+                                Text(name)
+                            }
+                        }
+                    }
+                    .searchable(text: $searchText) {
+                        ForEach(searchResults, id: \.self) { result in
+                            Text(result).searchCompletion(result)
+                        }
+                    }
+                    .navigationTitle("Items")
+                
+            }
         }
     }
 
@@ -41,7 +58,7 @@ struct ItemSearchView: View {
 
 struct ItemSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemSearchView()
+        ItemSearchView(recentlyDeleted: .constant(["coffee", "apple", "tomato"]))
             .preferredColorScheme(.light)
     }
 }
--- Leyaa/Components/SideMenu RoomListView/ProfilePageView.swift
+++ Leyaa/Components/SideMenu RoomListView/ProfilePageView.swift
@@ -20,67 +20,85 @@ struct ProfilePageView: View {
     var body: some View {
         
         ZStack {
+            NavigationView {
+                VStack {
 
-            VStack {
-
-                VStack{
-                    HStack {
-                        Image(selectedAvatar).resizable().frame(width: 300, height: 300).padding(.top, 40).padding(.bottom, -60)
-                    }.ignoresSafeArea()
-                
-                    VStack {
-                        HStack{
-                            Text(viewModel.currentUser?.fullname ?? "").font(.title).fontWeight(.bold).foregroundColor(.white)
-                        }
+                    VStack{
                         HStack {
-                            Text(viewModel.currentUser?.email ?? "").font(.body).fontWeight(.light).foregroundColor(.white)
-                        }
-                    }
+                            Image(selectedAvatar).resizable().frame(width: 300, height: 300).padding(.top, 40).padding(.bottom, -60)
+                        }.ignoresSafeArea()
                     
-                    Spacer()
-                }
-            
-                
-            
-                Spacer()
-              
-                ScrollView{
-                    DisclosureGroup("Avatar", isExpanded: $isExpanded) {
                         VStack {
-                            LazyVGrid(columns: fiveColumnGrid) {
-                                    ForEach(assetName, id: \.self) { avatar in
-                                        Image(avatar).resizable().frame(width: 50, height: 50).padding()
-                                            .onTapGesture {
-                                                self.selectedAvatar = avatar
-                                            }
-                                    }
-                                
+                            HStack{
+                                Text(viewModel.currentUser?.fullname ?? "").font(.title).fontWeight(.bold).foregroundColor(.white)
+                            }
+                            HStack {
+                                Text(viewModel.currentUser?.email ?? "").font(.body).fontWeight(.light).foregroundColor(.white)
+                            }
+                        }.padding(.top, -90)
+                        
+                        Spacer()
+                    }
+                
+                    
+                
+                    Spacer()
+                  
+                    ScrollView{
+                        DisclosureGroup("Choose a new Avatar", isExpanded: $isExpanded) {
+                            VStack {
+                                LazyVGrid(columns: fiveColumnGrid) {
+                                        ForEach(assetName, id: \.self) { avatar in
+                                            Image(avatar).resizable().frame(width: 50, height: 50).padding()
+                                                .onTapGesture {
+                                                    self.selectedAvatar = avatar
+                                                }
+                                        }
+                                    
+                                }
+                            }
+                            
+                        }.font(.headline)
+                            
+                    }
+      
+                    HStack{
+                        if (isExpanded && selectedAvatar != viewModel.currentUser?.avatar) {
+                            Button {
+                                    viewModel.updateAvatar(userID: (viewModel.currentUser?.id)!, newAvatar: selectedAvatar)
+                            } label: {
+                                Text("Save")
                             }
                         }
-                        
-                    }.font(.headline)
+                    
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 30)
+                    .padding(.top, 20)
+
                 }
-                
-                HStack{
-                    if (isExpanded && selectedAvatar != viewModel.currentUser?.avatar) {
-                        Button {
-                                viewModel.updateAvatar(userID: (viewModel.currentUser?.id)!, newAvatar: selectedAvatar)
-                        } label: {
-                            Text("Save")
+                .padding(.horizontal, 20)
+                .onAppear { selectedAvatar = viewModel.currentUser?.avatar ?? defaultAvatar }
+                .toolbar {
+                    ToolbarItem {
+                        Button(action: {
+                            wantToSignOut.toggle()
+                        }, label: {
+                            Text("Sign Out")
+                        }).buttonStyle(.plain)
+                        .confirmationDialog("Are you sure?",
+                          isPresented: $wantToSignOut) {
+                          Button("Sign Out", role: .destructive) {
+                              viewModel.signOut()
+                          }
+                        } message: {
+                          Text("Sad to see you leave.")
                         }
                     }
-                
                 }
-                .padding(.horizontal, 10)
-                .padding(.bottom, 30)
-                .padding(.top, 20)
-
-            }
-            .padding(.horizontal, 20)
-            .onAppear {
-                selectedAvatar = viewModel.currentUser?.avatar ?? defaultAvatar
-            }
+            }.navigationBarBackButtonHidden(true)
         }
+       
         
         
         
@@ -89,8 +107,9 @@ struct ProfilePageView: View {
         
 
 
-//struct ProfilePageView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ProfilePageView(viewModel: .constant(AuthViewModel), wantToSignOut: false)
-//    }
-//}
+struct ProfilePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePageView(wantToSignOut: false, selectedAvatar: "coffee").environmentObject(AuthViewModel())
+            .preferredColorScheme(.dark)
+    }
+}
