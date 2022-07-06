4441887d422dccf0e6fc68ba5cc274b1e108e63a
Yash Mishra <yashmishra12@hotmail.com>
2022-07-06 15:27:41 -0400

    Minor UI Changes

--- Leyaa/Components/Room/Split Bill/SplitBillView.swift
+++ Leyaa/Components/Room/Split Bill/SplitBillView.swift
@@ -42,7 +42,7 @@ struct SplitBillView: View {
                 
                 
                 NavigationLink {
-                    BillCreateView(roomData: $roomData, memberAmount: Array(repeating: 0.0, count: roomData.members.count))
+                    BillCreateView(roomData: $roomData, memberAmount: Array(repeating: Double(100)/Double(roomData.members.count), count: roomData.members.count))
                 } label: {
                     Text("Add Bill").buttonStyle()
                 }.buttonStyle(.plain)
