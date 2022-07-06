0a973f70b0618f53b7012229fc612f8e3d399611
Yash Mishra <yashmishra12@hotmail.com>
2022-07-06 15:43:58 -0400

    Slider Removed.

--- Leyaa/Components/Room/Split Bill/MemberPaymentSliderView.swift
+++ Leyaa/Components/Room/Split Bill/MemberPaymentSliderView.swift
@@ -32,80 +32,29 @@ struct MemberPaymentSliderView: View {
             //Top Layer Without Manual Control
             HStack {
                 
-                //Avatar and Name
-                VStack{
+                //Avatar
                     Image(avatar).resizable()
                         .aspectRatio(contentMode: .fill)
                         .clipped()
-                        .frame(width: 50, height: 50).clipShape(Circle())
-                    
-                    Text(name).font(.caption)
-                    
-                }
+                        .frame(width: 50, height: 50)
+
+                //Name
+                Text(name).font(.body)
                 
                 Spacer()
                 
-                //Slider
-                VStack (alignment: .trailing) {
-                    if maxAmount>0 {
-                        
-                        HStack {
-                            Spacer()
-                            
-                            Slider(
-                                value: $currentAmount,
-                                in: 0...maxAmount,
-                                step: 1
-                            ) { Text("") }
-                        minimumValueLabel: { Text("") }
-                        maximumValueLabel: { Text("") }
-                        onEditingChanged: { data in
-                            if data == false {
-                                memberAmount[index] = currentAmount
-                                }
-                            }
-                            
-                            TextField("Amt.", value: $currentAmount, formatter: formatterAmount)
-                                .font(.footnote)
-                                .focused(isEditing)
-                                .multilineTextAlignment(.trailing)
-                                .keyboardType(.decimalPad)
-                                .autocapitalization(.none)
-                                .foregroundColor(.blue)
-                                .frame(minWidth: 35, idealWidth: 35, maxWidth: 80)
-                                .padding(.trailing, 5)
-                            }
-                        }
-                    
-                    else {
-                        
-                        HStack {
-                            Spacer()
-                            
-                            Slider(
-                                value: $currentAmount,
-                                in: 0.01...0.01,
-                                step: 0.00001
-                            )
-                                { Text("") }
-                                minimumValueLabel: { Text("") }
-                                maximumValueLabel: {
-                                    Text("X")
-                            }
-                            
-                        }.padding(.trailing, 10)
-                        
-                        
-                    }
-                }
-                .frame(width: screenWidth*0.80)
-
-
-
+                TextField("Amount", value: $currentAmount, formatter: formatterAmount)
+                    .font(.callout)
+                    .focused(isEditing)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+                    .autocapitalization(.none)
+                    .foregroundColor(.blue)
+                    .frame(minWidth: 50)
             }
                
         }
-        .padding(.horizontal, 10)
+        .padding(.horizontal, 15)
         
         .onAppear(perform: {
             viewModel.getProfileAvatar(userID: userID) { res in self.avatar = res }
@@ -120,9 +69,9 @@ struct MemberPaymentSliderView: View {
     }
 }
 
-
+//
 //struct MemberPaymentSliderView_Previews: PreviewProvider {
 //    static var previews: some View {
-//        MemberPaymentSliderView(userID: "1234", maxAmount: .constant(200), memberAmount: .constant([1.2, 1.5, 2.4]), currentAmount: .constant(40.0), index: 2, isEditing: .constant(false)).environmentObject(AuthViewModel())
+//        MemberPaymentSliderView(userID: "1234", maxAmount: .constant(200), memberAmount: .constant([1.2, 1.5, 2.4]), currentAmount: .constant(40.0), index: 2, isEditing: false).environmentObject(AuthViewModel())
 //    }
 //}
