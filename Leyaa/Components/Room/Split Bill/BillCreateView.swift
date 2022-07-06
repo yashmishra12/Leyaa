439cdcc64fd2bf33fe79b43c0252856fe19c3ab9
Yash Mishra <yashmishra12@hotmail.com>
2022-07-06 15:56:03 -0400

    Split Bill Slider Removed.

--- Leyaa/Components/Room/Split Bill/BillCreateView.swift
+++ Leyaa/Components/Room/Split Bill/BillCreateView.swift
@@ -19,7 +19,7 @@ struct BillCreateView: View {
 
     
     @Binding var roomData: Room
-    @State var billAmount: Double = 100
+    @State var billAmount: Double?
 
     @State var itemName: String = ""
     @EnvironmentObject var viewModel: AuthViewModel
@@ -108,7 +108,7 @@ struct BillCreateView: View {
                 HStack {
                     Button {
                         for index in 0..<roomData.members.count {
-                            memberAmount[index] = Double(billAmount) / Double(roomData.members.count)
+                            memberAmount[index] = Double(billAmount ?? 0) / Double(roomData.members.count)
                         }
                     } label: {
                         Text("Equal Split").buttonStyle()
@@ -132,7 +132,7 @@ struct BillCreateView: View {
                 }
                
             //MARK: - Remaining Balance Info
-            Text("Remaining Balance: \(String(format: "%.2f", billAmount-memberAmount.reduce(0, +)))")
+            Text("Remaining Balance: \(String(format: "%.2f", (billAmount ?? 0)-memberAmount.reduce(0, +)))")
                 .font(.caption)
                 .padding(.top, 15)
             
@@ -200,7 +200,7 @@ struct BillCreateView: View {
 
 struct BillCreateView_Previews: PreviewProvider {
     static var previews: some View {
-        BillCreateView(roomData: .constant(Room(id: "123", title: "Avent Ferry", newItems: [], members: [])), memberAmount: [0])
+        BillCreateView(roomData: .constant(Room(id: "123", title: "Avent Ferry", newItems: [], members: [])), billAmount: 90, memberAmount: [0])
     }
 }
 
